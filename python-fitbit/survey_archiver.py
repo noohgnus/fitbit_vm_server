4dcd57d44bf6c3f5a8c67e0216cc4600b1a9c8df
hallagon <anthonysunghoonkim@gmail.com>
2018-06-19 16:29:45 -0400

    survey archiver added

--- python-fitbit/survey_archiver.py
+++ python-fitbit/survey_archiver.py
@@ -12,6 +12,8 @@ import re
 import pytz
 from dateutil import tz
 
+import pandas as pd
+
 EST_TZ = pytz.timezone("US/Eastern")
 from_zone = tz.gettz('UTC')
 to_zone = tz.gettz('America/New_York')
@@ -22,25 +24,89 @@ USER = "fitbitter"
 PASSWORD = "yiGLeVihDHhQMJPo"
 DB = "mmcfitbit"
 
+get_checkin_url = "https://jhprohealth.herokuapp.com/polls/get_checkins_yesterday/"
+get_survey_url = "https://jhprohealth.herokuapp.com/polls/get_surveys_yesterday/"
+
 now = str(datetime.datetime.now())
 
 class SurveyData:
-    def __init__(self, uid, survey_string, submitted_at):
+    def __init__(self, uid, survey_string, submitted_at, weighted_scores):
         self.uid = uid
         self.survey_string = survey_string
         self.submitted_at = submitted_at
+        self.weighted_scores = weighted_scores
 
     def __repr__(self):
-        return "SurveyData(User: " + str(self.uid) + ', submitted_at: ' + str(self.submitted_at) + ', survey: ' + str(self.survey_string) + ")"
+        return "SurveyData(User: " + str(self.uid) + ', submitted_at: ' + str(self.submitted_at) + ', survey: ' + str(self.survey_string) + ", " + str(self.weighted_scores) + ")"
+
+class CheckinData:
+    def __init__(self, uid, checkin_type, time):
+        self.uid = uid
+        self.checkin_type = checkin_type
+        self.time = time
+
+    def __repr__(self):
+        return "CheckinData(User: " + str(self.uid) + ', time: ' + str(self.time) + ', type: ' + str(self.checkin_type) + ")"
 
 def main():
     survey_set = pull_yesterday_survey()
-    calculate_category_scores(survey_set[0].survey_string)
-    # insert_surveys(survey_set)
+    insert_surveys(survey_set)
+    checkin_set = pull_yesterday_checkin()
+    insert_checkins(checkin_set)
+
+def pull_yesterday_checkin():
+    print("Retrieving surveys from yesterday...")
+    r = requests.get(get_checkin_url)
+    print(r.text)
+
+    checkin_json = json.loads(r.text)
+    checkin_list = []
+    checkins = checkin_json["Checkins"]
+    for checkin in checkins:
+        checkin_time = checkin["time"]
+        if '.' not in checkin_time:
+            checkin_time = checkin_time[:-1] + ".000Z"
+        utc = datetime.datetime.strptime(checkin_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+        utc = utc.replace(tzinfo=from_zone)
+        # Convert time zone
+        est = utc.astimezone(to_zone).strftime("%Y-%m-%d %H:%M:%S")
+        print est
+
+        checkin_list.append(CheckinData(uid=checkin["userid"], time=est, checkin_type=checkin["ping_type"]))
+
+    # print(uid)
+    print(checkin_list)
+    print("\t...Done getting yesterday's checkins")
+
+    return checkin_list
+
+def insert_checkins(checkin_list):
+    insert_set = []
+    for checkin in checkin_list:
+        # datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        insert_set.append((checkin.uid, checkin.time,  checkin.checkin_type, datetime.datetime.now()))
+        
+
+    try:
+        connection = db.Connection(host=HOST, port=PORT,
+                                   user=USER, passwd=PASSWORD, db=DB)
+
+        dbhandler = connection.cursor()
+        stmt = "INSERT INTO PC_Checkin (user_id, checked_in, type, added_on) \
+            VALUES (%s, %s, %s, %s)"
+        dbhandler.executemany(stmt, insert_set)
+
+
+    except Exception as e:
+        print "EXCEPTION IN insert_checkins: " + str(e)
+
+    finally:
+        connection.commit()
+        connection.close()
 
 def pull_yesterday_survey():
     print("Retrieving surveys from yesterday...")
-    r = requests.get("http://localhost:8000/polls/get_surveys_yesterday/")
+    r = requests.get(get_survey_url)
     print(r.text)
 
     survey_json = json.loads(r.text)
@@ -56,7 +122,7 @@ def pull_yesterday_survey():
         est = utc.astimezone(to_zone).strftime("%Y-%m-%d %H:%M:%S")
         print est
 
-        survey_list.append(SurveyData(uid=survey["submit_user_id"], survey_string=survey["answer_sequence"], submitted_at=est))
+        survey_list.append(SurveyData(uid=survey["submit_user_id"], survey_string=survey["answer_sequence"], submitted_at=est, weighted_scores=calculate_category_scores(survey["answer_sequence"])))
 
     # print(uid)
     print(survey_list)
@@ -91,7 +157,7 @@ def calculate_category_scores(survey_string):
         avg_map = dict()
         avg_map["physical_functioning"] = (ra[3] + ra[4] + ra[5] + ra[6] + ra[7] + ra[8] + ra[9] + ra[10] + ra[11] + ra[12]) / float(10)
         avg_map["role_limited_physical"] = (ra[13] + ra[14] + ra[15] + ra[16]) / float(4)
-        avg_map["role_limited_physical"] = (ra[17] + ra[18] + ra[19]) / float(3)
+        avg_map["role_limited_emotional"] = (ra[17] + ra[18] + ra[19]) / float(3)
         avg_map["energy"] = (ra[23] + ra[27] + ra[29] + ra[31]) / float(4)
         avg_map["emotional_wellbeing"] = (ra[24] + ra[25] + ra[26] + ra[28] + ra[30]) / float(5)
         avg_map["social_functioning"] = (ra[20] + ra[32]) / float(2)
@@ -104,18 +170,9 @@ def calculate_category_scores(survey_string):
     for i in survey_string:
         answer_map[count] = int(i)
         count += 1
-
-
-
-
     recoded_answers = recode_choices(answer_map)
-    print(recoded_answers)
-
     weighted_map = weigh_categories(recoded_answers)
-    return 
-
-def pull_checkins_survey():
-    pass
+    return weighted_map
 
 
 def insert_surveys(survey_set):
@@ -123,15 +180,28 @@ def insert_surveys(survey_set):
     for survey in survey_set:
         dataset = list(survey.survey_string)
         # datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        insert_set.append((survey.uid, survey.submitted_at, str(datetime.datetime.now())) + tuple(dataset))
-        print(insert_set)
+        insert_set.append((survey.uid, survey.submitted_at, str(datetime.datetime.now()), \
+            survey.weighted_scores["physical_functioning"], survey.weighted_scores["role_limited_physical"], survey.weighted_scores["role_limited_emotional"], survey.weighted_scores["energy"], \
+            survey.weighted_scores["emotional_wellbeing"], survey.weighted_scores["social_functioning"], survey.weighted_scores["pain"], survey.weighted_scores["general_health"]) \
+            + tuple(dataset))
 
     try:
         connection = db.Connection(host=HOST, port=PORT,
                                    user=USER, passwd=PASSWORD, db=DB)
 
         dbhandler = connection.cursor()
-        stmt = "INSERT INTO PC_Surveys_QoL (user_id, submitted_at, added_on, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31, q32, q33, q34, q35, q36) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        stmt = "INSERT INTO PC_Surveys_QoL (user_id, submitted_at, added_on, \
+            physical_functioning, role_limited_physical, role_limited_emotional, \
+            energy, emotional_wellbeing, social_functioning, pain, general_health, \
+            q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, \
+            q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, \
+            q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, \
+            q31, q32, q33, q34, q35, q36) VALUES (\
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, \
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, \
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, \
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, \
+            %s, %s, %s, %s, %s, %s, %s)"
         dbhandler.executemany(stmt, insert_set)
 
 
@@ -142,6 +212,35 @@ def insert_surveys(survey_set):
         connection.commit()
         connection.close()
 
+def query_test_surveys():
+    try:
+        connection = db.Connection(host=HOST, port=PORT,
+                                   user=USER, passwd=PASSWORD, db=DB)
+
+        cursor = connection.cursor(db.cursors.DictCursor)
+        result = cursor.execute("SELECT * FROM PC_Surveys_QoL WHERE user_id = 74477")
+        
+        rows = cursor.fetchall()
+ 
+        for row in rows:
+            # print(row["submitted_at"] < datetime.datetime.now())
+            print(row)
+
+        result = cursor.execute("UPDATE PC_Surveys_QoL SET physical_functioning=%s WHERE user_id=2") % datetime.datetime.now()
+ 
+    except Exception as e:
+        print(e)
+ 
+    finally:
+        cursor.close()
+        connection.close()
+
+
+print("==========================================")
 main()
-# calculate_category_scores("322332899")
+print("Data as of: " + str(datetime.datetime.now()))
+print("==========================================")
+
+# pull_yesterday_checkin()
+# query_test_surveys()
 
